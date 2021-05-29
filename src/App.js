f33421d208a0919b90fe775030235ac63cf113fa
gindrianjafy <georges-antonio.indrianjafy@intercloud.com>
2021-05-29 13:28:52 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <header className="sticky top-0 bg-white border-b border-gray-200 z-30">
+        header
+      </header>
+
+      <main className="relative flex">
+      content
+      </main>
+    </div>
+  );
+}
+
+export default App;
